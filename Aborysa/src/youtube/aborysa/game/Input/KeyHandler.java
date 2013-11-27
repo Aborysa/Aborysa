34dab77421fb2ae17e1bc2ff3a8ff3cd2bb70fc3
jarle2003 <jarle2003@BJVNB1287.SKOLEN.LOCAL>
2013-11-27 22:38:19 +0100

    useless changes

--- Aborysa/src/youtube/aborysa/game/Input/KeyHandler.java
+++ Aborysa/src/youtube/aborysa/game/Input/KeyHandler.java
@@ -6,7 +6,9 @@ import org.lwjgl.input.Keyboard;
 public class KeyHandler{
 	
 	static ArrayList<KeyListener> listeners = new ArrayList<KeyListener>();
-	
+	static int[] keys;
+	static int[][] keyCodes;
+	static boolean[] down = new boolean[65535];
 	public static void addKeyListener(KeyListener listener){
 		listeners.add(listener);
 	}
@@ -14,8 +16,14 @@ public class KeyHandler{
 		listeners.remove(listener);
 	}
 	public static void checkKeys(){
+		keyCodes = new int[listeners.size()][];
+		int count = 0;
 		for(KeyListener i : listeners){
-			int[] l = i.getKeyCodes();
+			keyCodes[count] = i.getKeyCodes();
+			count++;
+		}
+		for(int i=0; i<keyCodes.length;i++){
+			
 		}
 	}
 	public static int getKey(){
